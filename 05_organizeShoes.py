685e7bf28afcf8408092bc1b71934aff710d5538
fernandezguille <fernandez.caruso.g@gmail.com>
2024-12-09 22:40:39 -0300

    4 Star solution added (along with performance tests)

--- 05_organizeShoes.py
+++ 05_organizeShoes.py
@@ -7,7 +7,29 @@ Your task is to help the elves pair all the boots of the same size having a left
 
 Note: You can have more than one pair of boots of the same size!
 """
+from timeit import timeit
+
+# Best performance solution
 def organizeShoes(shoes: list[dict]) -> list[int]:
+  organized_shoes: dict = {}
+  organized_list: list[int] = []
+  for elem in shoes:
+    foot: str = elem['type']
+    size: int = elem['size']
+    pair = 'R' if foot == 'I' else 'I'
+    if size not in organized_shoes:
+      organized_shoes[size] = {foot: 1, pair: 0}
+      continue
+    if foot not in organized_shoes[size]:
+      organized_shoes[size][foot] = 0
+    if organized_shoes[size][pair] > 0:
+      organized_list.append(size)
+      organized_shoes[size][pair] -= 1
+    else:
+      organized_shoes[size][foot] += 1
+  return organized_list
+
+def organizeShoes1(shoes: list[dict]) -> list[int]:
   organized_shoes: dict = {}
   organized_list: list[int] = []
   for elem in shoes:
@@ -23,13 +45,35 @@ def organizeShoes(shoes: list[dict]) -> list[int]:
       organized_list.extend([sizes] * min(feet.values()))
   return organized_list
 
-# Example
-print(organizeShoes([
+
+# Wrapper to execute the function and capture result
+def create_wrapper(name, *args, **kwargs):
+  def inner() -> None:
+    global result  # global variable to store result
+    result = name(*args, **kwargs)
+  return inner
+
+result = None
+args = ([
   { "type": 'I', "size": 38 },
   { "type": 'I', "size": 38 },
   { "type": 'R', "size": 38 },
   { "type": 'R', "size": 38 },
+  { "type": 'I', "size": 38 },
   { "type": 'I', "size": 42 },
   { "type": 'R', "size": 41 },
   { "type": 'R', "size": 42 },
-]))
+],)
+
+# Warmup
+wrapper = create_wrapper(organizeShoes, *args)
+execution_time = timeit(wrapper, number=100000)
+
+# Performance test
+for function_name in (organizeShoes, organizeShoes1):
+  wrapper = create_wrapper(function_name, *args)
+  times = 100_000
+  print(f"{function_name.__name__:} took average {timeit(wrapper, number=times) / times*1000:.8f} microsec. Result:\n{result}")
+
+# Example
+# print(organizeShoes(args))
